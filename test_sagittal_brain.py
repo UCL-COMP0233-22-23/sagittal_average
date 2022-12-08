6746cc91a5c42400e0074819ce57c85b1ea25a7f
Shameer <shameer.ahmad.19@ucl.ac.uk>
2022-12-08 01:26:23 +0000

    fixed typos

--- test_sagittal_brain.py
+++ test_sagittal_brain.py
@@ -6,15 +6,15 @@ import numpy as np
 
 
 sample = np.array([[0,0,0], [0,0,0], [1,1,1]])
-np.savetxt('test_brain_sample.csv', sample, delimiter=',', fmt='%i') #save as csv and integers
+np.savetxt('brain_sample.csv', sample, delimiter=',', fmt='%i') #save as csv and integers
 
 expected = np.asarray([[0, 0, 1]])
-np.savetxt('test_brain_expected.csv',expected, delimiter=',', fmt='%i') #save as csv and integers
+np.savetxt('brain_expected.csv',expected, delimiter=',', fmt='%i') #save as csv and integers
 
 
 
 
-subprocess.run(['python', 'sagittal_brain.py'], shell=True) #run the main py file and create the test_brain_average result file
+subprocess.run(['python', 'sagittal_brain.py'], shell=True) #run the main py file and create the brain_average result file
 
 read_output = np.loadtxt('brain_average.csv', delimiter=',') #read the file (ie convert to array)
 
