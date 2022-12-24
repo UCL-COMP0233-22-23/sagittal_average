e8e410fe358e6a76c468987d082e6a0b124d255a
Chongfeng Ling <ling.chongfeng@outlook.com>
2022-12-24 22:35:04 +0000

    add an entry point

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="Sagittal_Average",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
+    entry_points={
+        'console_scripts':['sagittal_average_run = Sagittal_Average.command:process']
+    }
 )
